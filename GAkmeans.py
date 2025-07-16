f61a8a8d75a31c4492c40486e76a5dbdf0bc5239
Katherine Moss <katherinemossdeveloper@gmail.com>
2025-07-16 15:26:49 -0400

    Update GAkmeans.py
    Changed call to GAutility

--- GAkmeans.py
+++ GAkmeans.py
@@ -17,10 +17,9 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from GAutility import load_and_preprocess_image
+from GAutility import load_and_preprocess_image, get_plot_color_objects
 
 
 # Function to extract features from the image using the pre-trained model
@@ -40,35 +39,39 @@ def extract_features(model, img_path):
 # Visualize both PG and CEX clusters on one plot with different colors for 3 clusters
 def visualize_clusters(pca, reduced_features, colors, image_files, centroids, image_folder, legend_handles):
 
-    # draw the PCA components
-    plt.figure(figsize=(10, 8))
-    scatter = plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=colors, s=50)
+    try:
 
-    # Label the plot with filenames (optional)
-    for i, file_path in enumerate(image_files):
-        image_string = f'{os.path.basename(file_path)}'
-        # image_string = "."
-        plt.text(reduced_features[i, 0], reduced_features[i, 1], image_string, fontsize=8)
+        # draw the PCA components
+        plt.figure(figsize=(10, 8))
+        plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=colors, s=50)
 
-    # Add a legend to explain the colors; draw the centroid X's.
-    plt.legend(handles=legend_handles, loc='upper right')
-    plt.scatter(centroids[:, 0], centroids[:, 1], c='lime', s=300, marker='X', label='Centroids')
+        # Label the plot with filenames (optional)
+        for i, file_path in enumerate(image_files):
+            image_string = f'{os.path.basename(file_path)}'
+            # image_string = "."
+            plt.text(reduced_features[i, 0], reduced_features[i, 1], image_string,
+                     fontsize=8, color='black')
 
-    # Add the axes labels.
-    plt.xlabel(f'PC1 ({pca.explained_variance_ratio_[0] * 100:.1f}% variance)')
-    plt.ylabel(f'PC2 ({pca.explained_variance_ratio_[1] * 100:.1f}% variance)')
+        # Add a legend to explain the colors; draw the centroid X's.
+        plt.legend(handles=legend_handles, loc='upper right')
+        plt.scatter(centroids[:, 0], centroids[:, 1], c='lime', s=300, marker='X', label='Centroids')
 
-    plt.title('K-Means for CEX and PG.')
-    plt.savefig(os.path.join(image_folder, "kmeans_plot.jpg"))
-    plt.show()
+        # Add the axes labels.
+        plt.xlabel(f'PC1 ({pca.explained_variance_ratio_[0] * 100:.1f}% variance)')
+        plt.ylabel(f'PC2 ({pca.explained_variance_ratio_[1] * 100:.1f}% variance)')
+
+        plt.title('K-Means for CEX and PG.')
+        plt.savefig(os.path.join(image_folder, "kmeans_plot.jpg"))
+        plt.show()
+
+    except Exception as e:
+        print(f"An error occurred in GAkmeans.visualize_clusters: {e}")
 
 
 # Function to perform K-Means clustering
-def kmeans_driver(model, num_clusters=2, file_paths="", image_folder=""):
+def kmeans_driver(model, num_clusters=4, file_paths="", image_folder=""):
     features = []
     colors = []
-    centroids_kmeans = []
-    labels_kmeans = []
     features_reduced = []
 
     try:
@@ -93,34 +96,19 @@ def kmeans_driver(model, num_clusters=2, file_paths="", image_folder=""):
         # Get cluster labels
         labels_kmeans = sklearn_kmeans_clustering.labels_
         centroids_kmeans = sklearn_kmeans_clustering.cluster_centers_
+        print(f'f labels_kmeans {labels_kmeans}')
 
         # straighten out the forward, backward slashes.
         normalized_files = [os.path.normpath(file_path) for file_path in file_paths]
 
-        # Set colors for each cluster
-        colors = ['purple' if label == 0 else
-                  'blue' if label == 1 else
-                  'darkorange' if label == 2 else
-                  'green' for label in labels_kmeans
-                  ]
-
-        # Create custom legend handles
-        legend_handles = [
-            mpatches.Patch(color='purple', label='Cluster 1'),
-            mpatches.Patch(color='blue', label='Cluster 2'),
-            mpatches.Patch(color='orange', label='Cluster 3'),
-            mpatches.Patch(color='green', label='Cluster 4')
-        ]
+        # create a list of colors for each label, in hex
+        colors, legend_handles = get_plot_color_objects(labels_kmeans, num_clusters)
 
         # Visualize clusters on one plot
         visualize_clusters(pca, features_reduced, colors, normalized_files,
                            centroids_kmeans, image_folder, legend_handles)
 
-        # Print clustering results
-        print("images clustering results:")
-
     except Exception as e:
         print(f"An error occurred in GAkmeans: {e}")
 
-    return colors, centroids_kmeans, labels_kmeans, features_reduced
-
+    return colors, features_reduced
